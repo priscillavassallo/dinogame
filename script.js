7ee324784cef3a6e5d2d38af89edd633a398b8b3
priscillavassallo <priscillavassallo96@gmail.com>
2025-11-10 17:48:13 -0300

    adiciona tratamento do bug do pulo múltiplo

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 const dino = document.querySelector('.dino');
 
+let isJumping = false;
+
 function handlekeyUp(event){
     if(event.keyCode === 32){
+        if (isJumping === false) {
         jump();
+        }
     }
 }
 
 function jump(){
     let position = 0;
-    let upInterval = setInterval (() => {
+
+    isJumping = true; 
+
+    let upInterval = setInterval (() => { 
         if(position >= 150){
             clearInterval(upInterval);
-            //descendo
+            // descendo
             let downInterval = setInterval (() => {
                 if(position <= 0){
                     clearInterval(downInterval);
+                    isJumping = false;
                 }else{
                     position -= 20;
                     dino.computedStyleMap.bottom = position + 'px';
                 }
             }, 20);
         }else{
-            //subindo
-            position +=20;
+            // subindo
+            position += 20;
             dino.computedStyleMap.bottom = position + 'px'
-            
         }
     }, 20);
 }
