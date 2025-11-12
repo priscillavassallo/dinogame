d7bda772d348aaee83a899da3db18f0f7d427efa
priscillavassallo <priscillavassallo96@gmail.com>
2025-11-12 01:37:32 -0300

    adiciona detecção de colisão

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
+let position = 0;
 let isJumping = false;
 
 function handlekeyUp(event){
@@ -11,8 +12,6 @@ function handlekeyUp(event){
 }
 
 function jump(){
-    let position = 0;
-
     isJumping = true; 
 
     let upInterval = setInterval (() => { 
@@ -31,7 +30,7 @@ function jump(){
         }else{
             // subindo
             position += 20;
-            dino.style.bottom = position + 'px'
+            dino.style.bottom = position + 'px';
         }
     }, 20);
 }
@@ -49,6 +48,10 @@ function createCactus() {
         if (cactusPosition < -60) {
             clearInterval(leftInterval);
             background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            // game over
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
         } else {
             cactusPosition -= 10;
             cactus.style.left = cactusPosition + 'px';
